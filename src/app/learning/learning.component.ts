744068d9873a65c16324c8c6484959cca9062623
komukomo <komukomo@users.noreply.github.com>
2017-03-04 18:40:32 +0000

    Use mock-cards on '/learning' page

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../card';
+import { CardService } from '../card.service';
 
 @Component({
   selector: 'app-learning',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learning.component.css']
 })
 export class LearningComponent implements OnInit {
+  cards: Card[];
+  index = 0;
 
-  constructor() { }
+  constructor(private cardService: CardService) { }
+
+  getCards() {
+    this.cardService.getCards().then(
+      cards => this.cards = cards
+    );
+  }
 
   ngOnInit() {
+    this.getCards();
+  }
+
+  changeIndex(i: number) {
+    this.index = (i) % this.cards.length;
+  }
+
+  next() {
+    changeIndex(this.index + 1);
+  }
+
+  prev() {
+    changeIndex(this.index - 1);
   }
 
 }
