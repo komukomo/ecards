8b1735a8fc2c28c0bb5c0c18b7585e3329d26051
komukomo <komukomo@users.noreply.github.com>
2017-03-05 23:48:16 +0000

    Add levels

--- src/app/mock-cards.ts
+++ src/app/mock-cards.ts
@@ -1,16 +1,16 @@
 import { Card } from './card';
 
 export const CARDS: Card[] = [
-  {id: 1, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 2, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 3, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 4, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 5, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 6, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 7, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 8, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 9, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 10, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 11, front: 'hogehogehoge', back: "fugafugafugafuga"},
-  {id: 12, front: 'hogehogehoge', back: "fugafugafugafuga"},
+  {id: 1,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 0},
+  {id: 2,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 0},
+  {id: 3,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 1},
+  {id: 4,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 1},
+  {id: 5,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 0},
+  {id: 6,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 7},
+  {id: 7,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 7},
+  {id: 8,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 0},
+  {id: 9,  front: 'hogehogehoge', back: "fugafugafugafuga", level: 3},
+  {id: 10, front: 'hogehogehoge', back: "fugafugafugafuga", level: 3},
+  {id: 11, front: 'hogehogehoge', back: "fugafugafugafuga", level: 4},
+  {id: 12, front: 'hogehogehoge', back: "fugafugafugafuga", level: 4},
 ];
