cf392f10d1308fea24565f34c5504ba5bf58ba64
komukomo <komukomo@users.noreply.github.com>
2017-03-09 17:43:40 +0000

    Allow to edit contents of cards on the 'cards' page

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -10,17 +10,37 @@ import { CardService } from '../card.service';
 })
 export class CardsComponent implements OnInit {
   cards: Card[];
+  editingF: boolean[];
+  editingB: boolean[];
 
   constructor(private cardService: CardService) { }
 
   getCards() {
-    this.cardService.getCards().then(
-      cards => this.cards = cards
-    );
+    this.cardService.getCards().then((cards) => {
+      this.cards = cards;
+      this.editingF = Array(this.cards.length).fill(false);
+      this.editingB = Array(this.cards.length).fill(false);
+    });
   }
 
   ngOnInit() {
     this.getCards();
+  }
+
+  editStyle(i: number, isFront: boolean = true): boolean {
+    return isFront ? this.editingF[i]: this.editingB[i];
+  }
+
+  editCard(i: number, isFront: boolean = true) {
+    isFront ?
+      this.editingF[i] = true:
+      this.editingB[i] = true;
+  }
+
+  finEdit(i: number, isFront: boolean = true) {
+    isFront ?
+      this.editingF[i] = false:
+      this.editingB[i] = false;
   }
 
 }
