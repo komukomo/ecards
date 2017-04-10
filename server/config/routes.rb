0f0d2bc54a557cfd79ef01e397221a82bf8f47cd
komukomo <komukomo@users.noreply.github.com>
2017-04-10 06:43:21 +0000

    Route cards as API

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :cards
+  scope :api do
+    resources :cards
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
