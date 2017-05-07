6a535b20e7e0e3c3d7e4397650e38317e62545a5
komukomo <komukomo@users.noreply.github.com>
2017-05-07 15:52:49 +0000

    Set encoding

--- server/app/controllers/uploader_controller.rb
+++ server/app/controllers/uploader_controller.rb
@@ -1,7 +1,7 @@
 class UploaderController < ApplicationController
   def upload
     upload_file = params[:file]
-    parse(upload_file.read).each do |l|
+    parse(upload_file.read.force_encoding('utf-8')).each do |l|
       c = Card.new(front: l[0], front_sup: l[1], back:l[2], back_sup:l[3])
       c.save
     end
